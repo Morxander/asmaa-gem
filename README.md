cc69ab3b78001148deb47a0010b906c172948504
Morad Edwar <me@morad-edwar.com>
2016-06-22 10:18:47 +0200

    restructuring the code and fix the unknown name status

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ After checking out the repo, run `bin/setup` to install dependencies. Then, run 
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
+To add more names please add it to the `names.csv` file not to the binary `names.db` file and I will add them to the `db` file in the next version.
+
 ## Contributing
 
 Bug reports and pull requests are welcome on GitHub.
--- lib/asmaa.rb
+++ lib/asmaa.rb
@@ -6,37 +6,23 @@ module Asmaa
   @db = SQLite3::Database.new File.join(File.dirname(File.expand_path(__FILE__)), 'asmaa.db')
   
   def self.get_gender name
-    first_name = name.split()[0]
     query = @db.prepare "SELECT * FROM names WHERE first_name=?"
+    first_name = name.split()[0]
     query.bind_param 1, first_name
-    result = query.execute
-    result.next[0]
+    result = query.execute.next
+    unless result.nil?
+      result[0]
+    else
+      "unknown"
+    end
   end
 
   def self.is_male? name
-    is_male = false
-    first_name = name.split()[0]
-    query = @db.prepare "SELECT * FROM names WHERE first_name=?"
-    query.bind_param 1, first_name
-    result = query.execute
-    gender = result.next[0]
-    if gender == "male"
-      is_male = true
-    end
-    is_male
+    self.get_gender(name) == 'male'
   end
 
   def self.is_female? name
-    is_female = false
-    first_name = name.split()[0]
-    query = @db.prepare "SELECT * FROM names WHERE first_name=?"
-    query.bind_param 1, first_name
-    result = query.execute
-    gender = result.next[0]
-    if gender == "female"
-      is_female = true
-    end
-    is_female
+    self.get_gender(name) == 'female'
   end
 
 end
